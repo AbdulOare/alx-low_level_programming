0581e89803829a4279f9f40be8747f2c94d98270
AbdulOare <abdul.oare@gmail.com>
2022-10-13 21:57:15 +0100

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print with put
+ * 
+ * Return: Always 0 (Confirmed)
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
