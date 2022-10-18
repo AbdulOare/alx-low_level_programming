30502278f00d24fd308912fa55282ca9bf15e7b0
AbdulOare <abdul.oare@gmail.com>
2022-10-18 20:41:19 +0100

    2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,22 @@
-#include <
+#include "main.h"
+
 /**
- * Main
- * Description: a program that prints _putchar, followed by a new line.
- * Return: Always 0 (success)
- */
-void myPrint()
+ * main - This program prints _putchar, and a new line.
+ *
+ * Return: Always 0 (Success)
+**/
+
+int main(void)
 {
+	int i;
+	char c[] = "_putchar";
 
+	for (i = 0; i <= 7; i++)
+	{
+		_putchar(c[i]);
+	}
 
+	_putchar('\n');
+
+	return (0);
 }
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - This program checks for a lowercase character
+ * @c: This is the character to be checked
+ * Return: 1 if is lowercase & 0 otherwise
+**/
+
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
